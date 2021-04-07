4e43c39d3c4278215f8bc981dd33d15e28c0cff7
2k7749 <32010943+2k7749@users.noreply.github.com>
2021-04-07 17:35:45 +0700

    Update Action Grab and boost

---
+++ actions/boost.php
@@ -0,0 +1,163 @@
+<?php
+$typeBoost = $_GET['typeboost'];
+$refHeader = $_GET['refheader'];
+$userAgents = $_GET['ua'];
+$socksDame = $_GET['socksdame'];
+$siteBoost = $_GET['siteboost'];
+$extraLinkBoost = $_GET['extralinkboost'];
+
+if($typeBoost == 'singlethread')
+{
+//    echo $typeBoost.' | ';
+//    echo $refHeader.' | ';
+//    echo $userAgents.' | ';
+//    echo $socksDame.' | ';
+//    echo $siteBoost.' | ';
+    $proxyauth = 'user:password'; ////PRIVATE proxy
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_PROXY, $socksDame);
+    curl_setopt($ch, CURLOPT_USERAGENT, $userAgents);
+    curl_setopt($ch, CURLOPT_URL, $siteBoost);
+    curl_setopt($ch, CURLOPT_FAILONERROR, true); // Req HTTP respoinse
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); //redirects then go to the final redirected URL.
+    curl_setopt($ch, CURLOPT_REFERER, $refHeader);
+    curl_setopt($ch, CURLOPT_ENCODING, "gzip,deflate");
+    curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // Display screen need echo to display
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0); //The number of seconds to wait while trying to connect. Use 0 to wait indefinitely. (fix php after 30 seconds by default)
+    curl_setopt($ch, CURLOPT_TIMEOUT, 60); //timeout in seconds
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // don't verify ssl certificates, allows https scraping
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false); // don't verify ssl host, allows https scraping
+    $result = curl_exec($ch);
+    if ($result === false) {
+        $arr_data = array(
+            "result" => array(
+                "success" => false,
+                "socks" => $socksDame,
+                "referer" => $refHeader,
+                "message" => curl_error($ch)
+            )
+        );
+        echo json_encode($arr_data);
+      } else {
+        $arr_data = array(
+            "result" => array(
+                "success" => true,
+                "socks" => $socksDame,
+                "referer" => $refHeader,
+                "message" => 'Cool! Payload success'
+            )
+        );
+        echo json_encode($arr_data);
+      
+        }
+    curl_close($ch);
+	var_dump($result);
+}
+elseif($typeBoost == 'multithread')
+{
+    $req = array();
+    $extraLink = explode(",", $extraLinkBoost);
+    $mch = curl_multi_init();
+    
+    foreach($extraLink as $exkey => $exlinkvalue) //ex key and ex value
+    {
+        $req[$exkey] = array();
+        $req[$exkey]['url'] = $exlinkvalue;
+        $req[$exkey]['curl_handle'] = curl_init($exlinkvalue); // create normal cURL handle
+        //CONFIG OPTIONS FOR cURL
+        curl_setopt($req[$exkey]['curl_handle'], CURLOPT_PROXY, $socksDame);
+        curl_setopt($req[$exkey]['curl_handle'], CURLOPT_USERAGENT, $userAgents);
+        curl_setopt($req[$exkey]['curl_handle'], CURLOPT_FAILONERROR, true); // Req HTTP respoinse
+        curl_setopt($req[$exkey]['curl_handle'], CURLOPT_FOLLOWLOCATION, true); //redirects then go to the final redirected URL.
+        curl_setopt($req[$exkey]['curl_handle'], CURLOPT_REFERER, $refHeader);
+        curl_setopt($req[$exkey]['curl_handle'], CURLOPT_ENCODING, "gzip,deflate");
+        curl_setopt($req[$exkey]['curl_handle'], CURLOPT_AUTOREFERER, true);
+        curl_setopt($req[$exkey]['curl_handle'], CURLOPT_RETURNTRANSFER, 1); // Display screen need echo to display
+        curl_setopt($req[$exkey]['curl_handle'], CURLOPT_HEADER, 0);
+        curl_setopt($req[$exkey]['curl_handle'], CURLOPT_CONNECTTIMEOUT, 0); //The number of seconds to wait while trying to connect. Use 0 to wait indefinitely. (fix php after 30 seconds by default)
+        curl_setopt($req[$exkey]['curl_handle'], CURLOPT_TIMEOUT, 60); //timeout in seconds
+        curl_setopt($req[$exkey]['curl_handle'], CURLOPT_SSL_VERIFYPEER, false); // don't verify ssl certificates, allows https scraping
+        curl_setopt($req[$exkey]['curl_handle'], CURLOPT_SSL_VERIFYHOST, false); // don't verify ssl host, allows https scraping
+    
+        //Add ever single curl to curl multile handle
+        curl_multi_add_handle($mch, $req[$exkey]['curl_handle']);
+    }
+
+    //Execute requests using curl_multi_exec solve block and 100% cpu
+    $executeCurl = false;
+    do {
+        curl_multi_exec($mch, $executeCurl);
+        curl_multi_select($mch); // Wait for curl multi connect
+    } while ($executeCurl); // set the variable to FALSE value once it has finished
+
+    $arr_data_res = array();
+    foreach($req as $reqkey => $reqval){
+        // curl_multi_remove_handle($mch, $reqval['curl_handle']);
+        // $req[$reqkey]['content'] = curl_multi_getcontent($reqval['curl_handle']);
+        // $req[$reqkey]['http_code'] = curl_getinfo($reqval['curl_handle'], CURLINFO_HTTP_CODE);
+        
+        $domain = parse_url($url, PHP_URL_HOST);
+        $curlErrorCode = curl_errno($req[$reqkey]['curl_handle']);
+        if ($curlErrorCode === 0) {
+            $info = curl_getinfo($req[$reqkey]['curl_handle']);
+            $info['url'] = trim($info['url']);
+            if ($info['http_code'] == 200) {
+                $content = curl_multi_getcontent($req[$reqkey]['curl_handle']);
+                //$res[$domain] = sprintf("#HTTP-OK %0.2f kb returned", strlen($content) / 1024);
+                    $arr_data = array(
+                                "result" => array(
+                                "success" => true,
+                                "socks" => $socksDame,
+                                "referer" => $refHeader,
+                                "message" => sprintf("#HTTP-OK %0.2f kb returned", strlen($content) / 1024)
+                            )
+                    );
+                    array_push($arr_data_res, $arr_data);
+                    // echo json_encode($arr_data_success[0]);
+                    //echo $arr_data_res;
+                    // sleep(3);
+
+                    
+            }else {
+                //$res[$domain] = "#HTTP-ERROR {$info['http_code'] }  for : {$info['url']}";
+                    $arr_data = array(
+                                "result" => array(
+                                "success" => false,
+                                "socks" => $socksDame,
+                                "referer" => $refHeader,
+                                "message" => "#HTTP-ERROR {$info['http_code'] }  for : {$info['url']}"
+                            )
+                    );
+                    array_push($arr_data_res, $arr_data);
+                   //echo json_encode($arr_data);
+            }
+        } else {
+            //$res[$domain] = sprintf("#CURL-ERROR %d: %s ", $curlErrorCode, curl_error($req[$reqkey]['curl_handle']));
+			    $arr_data = array(
+                    "result" => array(
+                    "success" => false,
+                    "socks" => $socksDame,
+                    "referer" => $refHeader,
+                    "message" => sprintf("#CURL-ERROR %d: %s ", $curlErrorCode, curl_error($req[$reqkey]['curl_handle']))
+                )
+            );
+            array_push($arr_data_res, $arr_data);
+        }
+        curl_multi_remove_handle($mch, $reqval['curl_handle']);
+
+        //close the handle.
+        curl_close($req[$reqkey]['curl_handle']);
+        flush();
+        ob_flush();
+    }
+    echo json_encode($arr_data_res);
+    curl_multi_close($mch);
+}
+else{
+    echo 'WE NEED PAYLOAD';
+}
+
+
+?>
---
+++ actions/grabproxy.php
@@ -0,0 +1,65 @@
+<?php
+function fetch_proxies()
+{
+    $source = file_get_contents('http://www.sslproxies.org/');
+    preg_match_all('/<tbody>(.*?)<\/tbody>/is', $source, $matches);
+    preg_match_all('/<tr>(.*?)<\/tr>/is', $matches[1][0], $matches);
+    $return = array();
+    foreach ($matches[1] as $key => $val) {
+        preg_match_all('/<td>(.*?)<\/td>/is', $val, $m);
+        $return[] = "{$m[1][0]}:{$m[1][1]}";
+    }
+    return $return;
+}
+
+function freeproxylist()
+{
+	$url='https://spys.one/';
+	$headers   = Array();
+	$headers[] = ':authority:hidemy.name';
+    $headers[] = ':method:GET';
+    $headers[] = ':path: /en/proxy-list/?type=s';
+    $headers[] = ':scheme: https';
+    $headers[] = 'accept-encoding: gzip, deflate, br';
+    $headers[] = 'accept-language: en-US,en;q=0.9,vi;q=0.8';
+    $headers[] = 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9';
+    $headers[] = 'cache-control: max-age=0';
+    $headers[] = 'cookie: __cfduid=ddacb79b13fc92711cc20ebda3083687c1616743787; t=213086173';
+    $headers[] = "sec-fetch-dest: document";
+	$headers[] = "sec-fetch-mode: navigate";
+	$headers[] = "sec-fetch-site: none";
+	$headers[] = "sec-fetch-user: ?1";
+	$headers[] = "upgrade-insecure-requests: 1";
+    $ch        = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_AUTOREFERER, false);
+    curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36 Edg/89.0.774.68");
+    curl_setopt($ch, CURLOPT_HTTPHEADER,$headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_SSLVERSION,CURL_SSLVERSION_DEFAULT);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_VERBOSE, 1);
+    $result = curl_exec($ch);
+    $info = curl_getinfo($ch);
+    curl_close($ch);
+}
+
+function proxy_db(){
+	$source = file_get_contents('http://proxydb.net/?protocol=https&country=');
+    preg_match_all('/<tbody>(.*?)<\/tbody>/is', $source, $matches);
+    preg_match_all('/<tr>(.*?)<\/tr>/is', $matches[1][0], $matches);
+    $return = array();
+    foreach ($matches[1] as $key => $val) {
+        preg_match_all('/<td>(.*?)<\/td>/is', $val, $m);
+		$temp_strip = strip_tags($m[0][0]);
+        $return[] = "{$temp_strip}";
+    }
+    return $return;
+}
+
+
+echo '<html>' . str_replace(',', '<br>',implode(',', proxy_db())) . '</html>';
+//echo '<html>' . str_replace(' ', '<br>', implode(' ', fetch_proxies())) . '</html>';
